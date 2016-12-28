1e3dad3035e0e088c38ea5c602a9a1a56a7cfaca
Junming1983 <cjm_5271@sina.com>
2016-12-28 17:38:17 +0800

    Add productadapter

---
+++ DBAgent/ProductAdapter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DBAgent
+{
+  public class ProductAdapter
+  {
+    private const string _connectionString = "Data Source=MyDatabase.sqlite";
+    private const string _tableName = "ProductSpecification";
+  }
+}
